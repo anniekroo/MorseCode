c44690814c855b2dcce11ce85e9d2b202e2a5c04
Annie Kroo <annie.kroo@students.olin.edu>
2018-02-13 12:21:17 -0500

    debugging

--- morseCodeSend.py
+++ morseCodeSend.py
@@ -1,26 +1,26 @@
 # Sending morse code signals through LED connected to the Raspi
 # timing - start off with a variable time, change length of time for bit
-# spacing -
+# spacing - 
 # set it high for two bits and low for two bits (each ltter is 1 bit)
 # one bit between letters and two bits between words
 
 import time;
-import RPi.GPIO as GPIO
+import RPI.GPIO as GPIO
 
 t = 0.5;	# seconds, time between symbols is 1 bit time, between letter is 3 bit time, figure out how long it takes
 max_length = 20;
 msg = "PRAVA SOS"
 
 
-MORSECODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
-        'D': '-..',    'E': '.',      'F': '..-.',
-        'G': '--.',    'H': '....',   'I': '..',
-        'J': '.---',   'K': '-.-',    'L': '.-..',
-        'M': '--',     'N': '-.',     'O': '---',
-        'P': '.--.',   'Q': '--.-',   'R': '.-.',
-     	'S': '...',    'T': '-',      'U': '..-',
-        'V': '...-',   'W': '.--',    'X': '-..-',
-        'Y': '-.--',   'Z': '--..',
+MORSECODE = {"A": ".-",     "B": "-...",   "C": "-.-.", 
+        "D": "-..",    "E": ".",      "F": "..-.",
+        "G": "--.",    "H": "....",   "I": "..",
+        "J": ".---",   "K": "-.-",    "L": ".-..",
+        "M": "--",     "N": "-.",     "O": "---",
+        "P": ".--.",   "Q": "--.-",   "R": ".-.",
+     	"S": "...",    "T": "-",      "U": "..-",
+        "V": "...-",   "W": ".--",    "X": "-..-",
+        "Y": "-.--",   "Z": "--..",
         }
 
 class Safeguards:
@@ -46,7 +46,7 @@ def encode_morse(word):
 	for char in word:
 		if char == " ":
 			morseword += " "
-		morseword += MORSECODE[char.upper()]
+		morseword = morseword + MORSECODE[char.upper()]
 	return morseword
 
 def delay(duration):
